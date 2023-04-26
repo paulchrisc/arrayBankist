384c0ab4f725857afc4382f5bb73b3c7810c494d
paulchrisc <128294135+paulchrisc@users.noreply.github.com>
2023-04-26 16:46:07 +0800

    array methods

--- script.js
+++ script.js
@@ -74,3 +74,38 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+
+console.log(arr.slice());
+//or
+console.log([...arr]);
+
+//splice - mutates original array. takes part of array
+
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+//reverse
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+//mutates
+console.log(arr2);
+
+//concat - does not mutate
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//join
+console.log(letters.join('-'));
